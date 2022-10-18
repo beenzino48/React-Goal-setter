18b0139826a2acdbd4423e743f282efdd8c19958
hamishliu <hamish.mc.liu@gmail.com>
2022-10-18 22:49:23 +0900

    readded logic so that the form changes if it is invalid or not, also set button so that the css is directly linked to the button's click

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
+import React, { isValidElement, useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  // useState for invalid user input
 
-  const goalInputChangeHandler = event => {
+  const [isValid, SetIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    // trim checks for white space when inputted
+    // reset error colors if input is valid
+    if (event.target.value.trim().length > 0) {
+      SetIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    // checks if user input is empty
+    // enteredValue is the prev state for the user input form (should be empty)
+    if (enteredValue.length === 0) {
+      return SetIsValid(false);
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
